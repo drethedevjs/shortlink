d5a044253ba38a5bc638addb1ab45d57698eaf78
athomas-wtv <athomas.wtv@gmail.com>
2021-02-24 01:21:22 -0500

    Added decoding method

--- shortLink/Controllers/ShortLinkController.cs
+++ shortLink/Controllers/ShortLinkController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ShortLink.Data;
+using ShortLink.Models;
 
 namespace shortLink.Controllers
 {
@@ -21,9 +22,38 @@ namespace shortLink.Controllers
         }
 
         [HttpPost, Route("encode")]
-        public ShortLinkPair Encode()
+        public ShortLinkPair Encode(string longLink)
         {
+            var pair = new ShortLinkPair()
+            {
+                LongLink = longLink,
+                ShortenedLink = $"http://short.est/{RandomString(6)}"
+            };
+            this.apiContext.AddPair(pair);
+            this.apiContext.SaveChanges();
             
+            var myList = this.apiContext.GetPairs();
+            foreach(var x in myList)
+            {
+                Console.WriteLine($"{x.Id} {x.LongLink} | {x.ShortenedLink}");
+            }
+            return pair;
+        }
+
+        [HttpPost, Route("decode")]
+        public IActionResult Decode(string shortenedLink)
+        {
+            Console.WriteLine("Hit!");
+            var pair = this.apiContext.GetPairByShortenedLink(shortenedLink);
+            Console.WriteLine($"Retrieved {pair.LongLink} using {shortenedLink}");
+            return RedirectToAction("Index", pair);
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
